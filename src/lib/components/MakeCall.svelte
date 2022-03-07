cc309ba4dc0ee20a521611f87bd3c32c329fe598
Alex van der Valk <avandervalk@twilio.com>
2022-03-07 16:44:33 +0000

    fix error

---
+++ src/lib/components/MakeCall.svelte
@@ -0,0 +1,29 @@
+<script>
+  import ColumnHeader from "./ColumnHeader.svelte";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let numbertoDial;
+
+  const makeCall = () => {
+    dispatch("dial", numbertoDial);
+  };
+</script>
+
+<div>
+  <ColumnHeader>Make a Call</ColumnHeader>
+  <form on:submit|preventDefault={makeCall}>
+    <label for="dial">Enter a phone number or client name</label>
+    <input id="dial" bind:value={numbertoDial} placeholder="+441234567890" />
+    <button type="submit">Call</button>
+  </form>
+</div>
+
+<style>
+  input {
+    width: 100%;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+  }
+</style>
